2307be5818ba6e79ed10926277d65dceb8b15069
mohammed-akkad <momoo362514@gmail.com>
2023-07-04 18:32:50 +0300

    complete trivia titans service

--- app/src/main/java/com/chocolate/triviatitans/data/remote/service/TriviaTitansService.kt
+++ app/src/main/java/com/chocolate/triviatitans/data/remote/service/TriviaTitansService.kt
@@ -1,4 +1,35 @@
 package com.chocolate.triviatitans.data.remote.service
 
+import com.chocolate.triviatitans.data.remote.response.dto.ImageChoiceQuestionsRemoteDto
+import com.chocolate.triviatitans.data.remote.response.dto.TextChoiceQuestionsRemoteDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TriviaTitansService {
+
+    /// region image choice question
+    @GET("questions")
+    suspend fun getImageChoiceQuestion(
+        @Query("limit") limit: Int,
+        @Query("categories") categories: String,
+        @Query("difficulties") difficulties: String,
+        @Query("types") types: String = "image_choice"
+    ): Response<ImageChoiceQuestionsRemoteDto>
+
+    /// endregion
+
+    /// region image choice question
+    @GET("questions")
+    suspend fun getTextChoiceQuestion(
+        @Query("limit") limit: Int,
+        @Query("categories") categories: String,
+        @Query("difficulties") difficulties: String,
+        @Query("types") types: String = "text_choice"
+    ): Response<TextChoiceQuestionsRemoteDto>
+
+    /// endregion
+
+
+
 }
