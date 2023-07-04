7877d97196fdbb884b4cd31912e2104352c32b3e
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-04 13:40:13 +0200

    add fonts and typogrophy

--- app/src/main/java/com/chocolate/triviatitans/ui/theme/Type.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/theme/Type.kt
@@ -2,33 +2,74 @@ package com.chocolate.triviatitans.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.chocolate.triviatitans.R
 
+val firaSansFamily = FontFamily(
+    Font(R.font.firasans_regular, FontWeight.Normal),
+    Font(R.font.firasans_semibold, FontWeight.SemiBold),
+    Font(R.font.firasans_medium, FontWeight.Medium),
+    Font(R.font.firasans_bold, FontWeight.Bold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = firaSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 28.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 28.sp,
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = firaSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 8.sp,
+        lineHeight = 12.sp,
+    ),
+
 )
