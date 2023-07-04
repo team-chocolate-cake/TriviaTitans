201ba0b0f54f9bdd1a99a1f3a9560d5515587423
Saif Alhaider <saifharith90@gmail.com>
2023-07-05 00:37:13 +0300

    add: header total bounces

---
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/components/TotalBounce.kt
@@ -0,0 +1,24 @@
+package com.chocolate.triviatitans.presentation.screens.quiz_screen.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.chocolate.triviatitans.R
+
+@Composable
+fun TotalBounce(bounce: Int) {
+        Row() {
+            Text(text = bounce.toString())
+            SpacerHorizontal4Dp()
+            Image(painter = painterResource(id = R.drawable.ic_score), contentDescription = "icon score")
+        }
+}
+
+@Preview
+@Composable
+fun TotalBouncePreview() {
+    TotalBounce(500)
+}
