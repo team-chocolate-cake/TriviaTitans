256d8334e302bfece6914f87cdb4426b6cd40067
mohammed-akkad <momoo362514@gmail.com>
2023-07-04 20:22:21 +0300

    add base reposistory

---
+++ app/src/main/java/com/chocolate/triviatitans/data/repository/BaseRepository.kt
@@ -0,0 +1,26 @@
+package com.chocolate.triviatitans.data.repository
+
+import retrofit2.Response
+import java.net.UnknownHostException
+
+abstract class BaseRepository {
+
+    protected suspend fun <T> wrapApiCall(call: suspend () -> Response<T>): T {
+        return try {
+            val result = call()
+            if (result.code() == UNAUTHORIZED_CODE) {
+                throw UnauthorizedThrowable()
+            }
+
+            result.body() ?: throw ParsingThrowable()
+        } catch (e: UnknownHostException) {
+            throw NoNetworkThrowable()
+        } catch (e: Exception) {
+            throw ApiThrowable(e.message)
+        }
+    }
+
+    private companion object {
+        const val UNAUTHORIZED_CODE = 401
+    }
+}
