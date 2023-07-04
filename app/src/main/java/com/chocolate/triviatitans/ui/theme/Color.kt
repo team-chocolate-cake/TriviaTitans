039489bbdd3eeb591db11e53873af32efa534e2c
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-04 13:54:34 +0200

    add colors from design system

--- app/src/main/java/com/chocolate/triviatitans/ui/theme/Color.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/theme/Color.kt
@@ -5,7 +5,28 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF0F062C)
+val DarkOnBackground87 = Color(0xDEF6F6F6)
+val DarkOnBackground60 = Color(0x99F6F6F6)
+val DarkOnBackground38 = Color(0x61F6F6F6)
+val DarkSecondary = Color(0xFF0E042C)
+val DarkCard = Color(0xFF483081)
+val DarkBorder = Color(0x149C99A1)
+
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFF5F5F5)
+val LightOnBackground87 = Color(0xDE121212)
+val LightOnBackground60 = Color(0x99121212)
+val LightOnBackground38 = Color(0x61121212)
+val LightSecondary = Color(0xFFEDECEF)
+val LightCard = Color(0xFFFFFFFF)
+val LightBorder = Color(0x14845EC2)
+
+
+val Primary = Color(0xFF371E71)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnSecondary = Color(0xFFE4D3FF)
--- app/src/main/java/com/chocolate/triviatitans/ui/theme/Theme.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/theme/Theme.kt
@@ -16,25 +16,20 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    onPrimary = OnPrimary,
+    secondary = DarkSecondary,
+    onSecondary = OnSecondary,
+    tertiary = Pink80,
+    background = DarkBackground,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    onPrimary = OnPrimary,
+    secondary = LightSecondary,
+    tertiary = Pink40,
+    background = LightBackground,
 )
 
 @Composable
