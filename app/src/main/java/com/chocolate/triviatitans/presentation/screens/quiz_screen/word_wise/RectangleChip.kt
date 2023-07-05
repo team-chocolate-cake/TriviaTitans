8d10a441b94fd0fd6a68b3a8d4dda1a55ededaa4
Mahmoud <Elbory>
2023-07-05 15:12:33 +0300

    remove file RectangleChip

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/RectangleChip.kt
+++
@@ -1,9 +0,0 @@
-package com.chocolate.triviatitans.presentation.screens.quiz_screen.word_wise
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun RectangleChip(){
-Text(text = "test")
-}
