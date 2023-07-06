dc26e5b24658b181de3be254fbc7ee6323a9eca4
Saif Alhaider <saifharith90@gmail.com>
2023-07-06 23:33:17 +0300

    edit: adjust spacing in QuizScreen

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/QuizScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/QuizScreen.kt
@@ -1,6 +1,8 @@
 package com.chocolate.triviatitans.presentation.screens.quiz_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -8,27 +10,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chocolate.triviatitans.composables.SpacerVertical16
+import com.chocolate.triviatitans.composables.SpacerVertical32
 import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.AnswersSection
 import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.Header
 import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.ProgressIndicator
+import com.chocolate.triviatitans.presentation.theme.TriviaCustomColors
+import com.chocolate.triviatitans.presentation.theme.TriviaTitansTheme
 
 @Composable
 fun QuizScreen() {
-    Column(Modifier.padding(horizontal = 16.dp)) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = TriviaCustomColors.current.background)
+            .padding(horizontal = 16.dp)
+    ) {
+        SpacerVertical16()
         Header()
+        SpacerVertical16()
         ProgressIndicator(progressPercentage = .6f)
+        SpacerVertical32()
         Text(
             text = "What sport is best known as the ‘king of sports ?",
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(top = 16.dp, bottom = 24.dp)
+            color = TriviaCustomColors.current.onBackground87
         )
+        SpacerVertical16()
         AnswersSection()
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview(showSystemUi = true,)
 @Composable
 fun QuizScreenPreview() {
-    QuizScreen()
+    TriviaTitansTheme() {
+        QuizScreen()
+    }
 }
 
