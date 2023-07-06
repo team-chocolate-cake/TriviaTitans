02b5baf51db1ed0b6cc1e1d3399cae069b3b81c5
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-06 21:12:22 +0300

    update configuration screen using new design system

--- app/src/main/java/com/chocolate/triviatitans/composables/ConfigurationCard.kt
+++ app/src/main/java/com/chocolate/triviatitans/composables/ConfigurationCard.kt
@@ -1,6 +1,5 @@
-package com.chocolate.triviatitans.composable
+package com.chocolate.triviatitans.composables
 
-import android.content.res.Resources.Theme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,21 +19,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chocolate.triviatitans.R
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
-import com.chocolate.triviatitans.ui.theme.TriviaTitansTheme
 
 @Composable
 fun ConfigurationCard(typeTitle:String,typeDescription:String,typeImage: Int,
 color: Color = TriviaCustomColors.current.card
 ){
-
 
        Card(modifier = Modifier
            .fillMaxWidth()
@@ -49,12 +44,17 @@ color: Color = TriviaCustomColors.current.card
                    .weight(0.5f)
                    .padding(start = 12.dp),
                    verticalArrangement = Arrangement.Center) {
-                   Text(text = typeTitle, fontFamily = MaterialTheme.typography.titleMedium.fontFamily)
+                   Text(text = typeTitle,
+                       fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+                   ,fontSize = 16.sp
+                   ,color = TriviaCustomColors.current.onBackground87)
 
                    Text(text = typeDescription
                        , maxLines = 3,
                        textAlign = TextAlign.Justify,
-                       fontFamily = MaterialTheme.typography.titleSmall.fontFamily)
+                       fontFamily = MaterialTheme.typography.bodyMedium.fontFamily
+                   ,fontSize = 12.sp
+                   ,color = TriviaCustomColors.current.onBackground60)
 
                }
                Spacer(Modifier.size( 24.dp))
--- app/src/main/java/com/chocolate/triviatitans/screens/ConfigurationScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/screens/ConfigurationScreen.kt
@@ -22,44 +22,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chocolate.triviatitans.R
-import com.chocolate.triviatitans.composable.ConfigurationCard
-import com.chocolate.triviatitans.ui.theme.Primary
-import com.chocolate.triviatitans.ui.theme.Purple80
+import com.chocolate.triviatitans.composables.ConfigurationCard
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
 import com.chocolate.triviatitans.ui.theme.TriviaTitansTheme
 
 @Composable
 fun ConfigurationScreen(){
+
+TriviaTitansTheme() {
     Column(modifier = Modifier
         .fillMaxSize()
-        .background(Color(0xF1F1F1F1))
+        .background(TriviaCustomColors.current.background)
         .padding(16.dp)) {
-TriviaTitansTheme() {
-
-    Text(text = "Game Type", modifier = Modifier.padding(vertical = 8.dp),
-        fontFamily = MaterialTheme.typography.titleMedium.fontFamily)
+    Text(text = stringResource(id = R.string.game_type), modifier = Modifier.padding(vertical = 8.dp),
+        fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+    , fontSize = 16.sp
+    , color = TriviaCustomColors.current.onBackground87)
 
 
     ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card1)
         , typeDescription = stringResource(id = R.string.config_content_card1)
-        , typeImage =R.drawable.configration_multi_choice_icon)
+        , typeImage =R.drawable.configuratoin_multi_choice_icon)
 
     ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card2)
         , typeDescription = stringResource(id = R.string.config_content_card2)
-        , typeImage =R.drawable.configration_multi_choice_images_icon )
+        , typeImage =R.drawable.configuratoin_multi_choice_images_icon )
 
     ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card3)
         , typeDescription = stringResource(id = R.string.config_content_card3)
-        , typeImage =R.drawable.configration_word_wise_icon )
+        , typeImage =R.drawable.configuratoin_word_wise_icon )
 
 
     Card(modifier = Modifier
@@ -75,18 +73,25 @@ TriviaTitansTheme() {
                 .weight(0.5f)
                 .padding(start = 12.dp),
                 verticalArrangement = Arrangement.Center) {
-                Text(text = stringResource(id = R.string.config_title_card4), fontFamily = MaterialTheme.typography.titleMedium.fontFamily)
+                Text(text = stringResource(id = R.string.config_title_card4)
+                    , fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+                ,fontSize = 16.sp
+                ,color = TriviaCustomColors.current.primary)
 
                 Text(text = stringResource(id = R.string.config_content_card4)
                     , maxLines = 3,
                     textAlign = TextAlign.Justify,
-                    fontFamily = MaterialTheme.typography.titleSmall.fontFamily)
+                    fontFamily = MaterialTheme.typography.titleLarge.fontFamily
+                ,fontSize = 22.sp
+                ,color = TriviaCustomColors.current.primary)
 
             }
             Spacer(Modifier.size( 24.dp))
             Image(modifier = Modifier
                 .fillMaxHeight()
-                .padding(end = 12.dp),painter = painterResource(id = R.drawable.configuration_coming_soon_icon), contentDescription =null )
+                .padding(end = 12.dp)
+                ,painter = painterResource(id = R.drawable.configuratoin_coming_soon_icon),
+                contentDescription =null )
         }
     }
 
@@ -95,10 +100,12 @@ TriviaTitansTheme() {
     Button(onClick = { /*TODO*/ }, modifier = Modifier
         .width(250.dp)
         .align(Alignment.CenterHorizontally)
-        .padding(38.dp),
-        colors = ButtonDefaults.buttonColors(Primary)
+        .padding(top=38.dp),
+        colors = ButtonDefaults.buttonColors(TriviaCustomColors.current.primary)
     ) {
-        Text(text = "Play Now",fontFamily = MaterialTheme.typography.titleMedium.fontFamily)
+        Text(text = stringResource(id = R.string.play_now),
+            fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+        ,fontSize = 20.sp)
 
     }
 
