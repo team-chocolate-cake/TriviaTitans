d0d942d2588456e9a3278f14406c54b2d5c1f6ee
Saif Alhaider <saifharith90@gmail.com>
2023-07-06 23:32:01 +0300

    add: ui theme to TotalBounce component

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/components/TotalBounce.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/components/TotalBounce.kt
@@ -7,11 +7,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.chocolate.triviatitans.R
+import com.chocolate.triviatitans.presentation.theme.TriviaCustomColors
 
 @Composable
 fun TotalBounce(bounce: Int) {
         Row() {
-            Text(text = bounce.toString())
+            Text(text = bounce.toString(),color = TriviaCustomColors.current.onBackground87)
             SpacerHorizontal4Dp()
             Image(painter = painterResource(id = R.drawable.ic_score), contentDescription = "icon score")
         }
