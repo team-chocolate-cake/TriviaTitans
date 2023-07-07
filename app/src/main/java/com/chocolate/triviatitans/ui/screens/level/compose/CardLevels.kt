f111132b83ae6251b8a38e4d11b14000553670c7
mohammed-akkad <momoo362514@gmail.com>
2023-07-07 19:52:11 +0300

    clean up

--- app/src/main/java/com/chocolate/triviatitans/ui/screens/level/compose/CardLevels.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/screens/level/compose/CardLevels.kt
@@ -1,5 +1,15 @@
 package com.chocolate.triviatitans.ui.screens.level.compose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.chocolate.triviatitans.ui.theme.CustomColorsPalette
+
 @Composable
 fun CardLevels(color: CustomColorsPalette) {
     Row(
