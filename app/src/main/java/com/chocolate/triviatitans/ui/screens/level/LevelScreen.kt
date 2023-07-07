0438e4a191a43e43afef923841735f60804b4151
mohammed-akkad <momoo362514@gmail.com>
2023-07-08 01:19:38 +0300

    edit level screen

--- app/src/main/java/com/chocolate/triviatitans/ui/screens/level/LevelScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/screens/level/LevelScreen.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -12,10 +14,13 @@ import com.chocolate.triviatitans.ui.screens.level.compose.AppBar
 import com.chocolate.triviatitans.ui.screens.level.compose.CardLevels
 import com.chocolate.triviatitans.ui.screens.level.compose.DescriptionLevel
 import com.chocolate.triviatitans.ui.screens.level.compose.LevelImage
+import com.chocolate.triviatitans.ui.screens.level.compose.Score
 import com.chocolate.triviatitans.ui.screens.level.compose.StartGameButton
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
 import com.chocolate.triviatitans.ui.theme.TriviaTitansTheme
-import com.chocolate.triviatitans.view_model.LevelViewModel
+import com.chocolate.triviatitans.view_model.level.LevelUiState
+import com.chocolate.triviatitans.view_model.level.LevelViewModel
+import com.chocolate.triviatitans.view_model.level.TypeLevel
 
 
 @Composable
@@ -23,21 +28,33 @@ fun LevelScreen(
     navController: NavController,
     viewModel: LevelViewModel = hiltViewModel()
 ) {
+    val state by viewModel.state.collectAsState()
     LevelContent(
         onClickBack = { navController.popBackStack() },
-        onClickStartGame = {}
+        onClickStartGame = {},
+        onLevelSelected = viewModel::updateSelectedLevel,
+        state = state,
     )
 }
 
 @Composable
-fun LevelContent(onClickBack: () -> Unit, onClickStartGame: () -> Unit) {
+fun LevelContent(
+    onClickBack: () -> Unit,
+    onClickStartGame: () -> Unit,
+    onLevelSelected: (TypeLevel) -> Unit,
+    state: LevelUiState,
+) {
     val colors = TriviaCustomColors.current
     Column(modifier = Modifier.fillMaxSize()) {
         AppBar(onClickBack = onClickBack, color = colors)
         LevelImage()
+        Score(state, colors)
         DescriptionLevel(color = colors)
-        CardLevels(color = colors)
-        StartGameButton(color = colors, onClickStartGame = onClickStartGame)
+        CardLevels(color = colors, onLevelSelected = onLevelSelected, state = state)
+        StartGameButton(
+            color = colors,
+            onClickStartGame = onClickStartGame,
+        )
     }
 }
 
@@ -50,7 +67,9 @@ fun MyPreview() {
     TriviaTitansTheme() {
         LevelContent(
             onClickBack = {},
-            onClickStartGame = {}
+            onClickStartGame = {},
+            onLevelSelected = {},
+            state = LevelUiState(),
         )
     }
 }
