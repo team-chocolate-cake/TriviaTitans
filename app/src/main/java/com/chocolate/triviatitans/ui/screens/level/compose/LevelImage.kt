5a29dc73e952b92c624b3d7d4d2778d0204f6138
mohammed-akkad <momoo362514@gmail.com>
2023-07-07 20:29:51 +0300

    edit level image

--- app/src/main/java/com/chocolate/triviatitans/ui/screens/level/compose/LevelImage.kt
+++ app/src/main/java/com/chocolate/triviatitans/ui/screens/level/compose/LevelImage.kt
@@ -1,6 +1,13 @@
 package com.chocolate.triviatitans.ui.screens.level.compose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.chocolate.triviatitans.R
 
 @Composable
 fun LevelImage() {
