a9c6b993ca5070236dde64d4899fc2e47ca4f906
mohammed-akkad <momoo362514@gmail.com>
2023-07-07 19:51:15 +0300

    add view model for level screen

---
+++ app/src/main/java/com/chocolate/triviatitans/view_model/LevelViewModel.kt
@@ -0,0 +1,12 @@
+package com.chocolate.triviatitans.view_model
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LevelViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
