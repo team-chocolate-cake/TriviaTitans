2c9f8daefe0d476eed923ab70af85a1a3de445b6
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-07-07 15:05:02 +0300

    update category screen

--- app/src/main/java/com/chocolate/triviatitans/screen/category/CategoryScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/screen/category/CategoryScreen.kt
@@ -1,7 +1,5 @@
 package com.chocolate.triviatitans.screen.category
 
-import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,16 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,101 +24,146 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.chocolate.triviatitans.R
-import com.chocolate.triviatitans.composable.CategoryCard
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.chocolate.triviatitans.screen.category.composable.CategoryCard
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
-import com.chocolate.triviatitans.ui.theme.Typography
+import com.chocolate.triviatitans.ui.theme.TriviaTitansTheme
+import com.chocolate.triviatitans.viewmodel.category.CategoriesUiState
+import com.chocolate.triviatitans.viewmodel.category.CategoryUiState
+import com.chocolate.triviatitans.viewmodel.category.CategoryViewModel
 
 
 @Composable
-fun CategoryScreen() {
-    val categories = listOf(
-        "Geography",
-        "Music",
-        "Sport and leisure",
-        "Movie and Tv",
-        "Arts and literature",
-        "History",
-        "Society and culture",
-        "Science",
-        "General Knowledge",
-        "Food And Drink"
-    )
+fun CategoryScreen(
+    viewModel: CategoryViewModel = hiltViewModel()
+) {
 
-    CategoryContent(categories)
+    val state by viewModel.state.collectAsState()
+
+    CategoryContent(
+        state,
+        onCategorySelected = { category ->
+            viewModel.onCategorySelected(category)
+            viewModel.updateSelectedCategories()
+        }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CategoryContent(categories: List<String>) {
+fun CategoryContent(
+    state: CategoriesUiState,
+    onCategorySelected: (CategoryUiState) -> Unit = {}
+) {
     val colors = TriviaCustomColors.current
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        containerColor =colors.background,
+        containerColor = colors.background,
         topBar = {
-        TopAppBar(
-            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = colors.background),
-            title = { Text(text = "You want to improve today?",style = MaterialTheme.typography.titleMedium, color = colors.onBackground87) },
-            navigationIcon = {
-                IconButton(onClick = {/*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Go Back" ,tint = colors.onBackground87)
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(colors.background),
+                title = {
+                    Text(
+                        text = "You want to improve today?",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = colors.onBackground87
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {/*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Go Back",
+                            tint = colors.onBackground87
+                        )
+                    }
                 }
-            }
-        )
-    },
+            )
+        },
         bottomBar = {
-            Surface(modifier = Modifier
-                .height(80.dp)
-                .fillMaxWidth(),
-                color =colors.card,
+            Surface(
+                modifier = Modifier
+                    .height(80.dp)
+                    .fillMaxWidth(),
+                color = colors.card,
                 shadowElevation = 20.dp,
                 tonalElevation = 20.dp,
-                shape = RoundedCornerShape(topStart = 24.dp , topEnd = 24.dp)
+                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
             ) {
-                Row(modifier = Modifier.padding(16.dp) , horizontalArrangement = Arrangement.SpaceBetween) {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Total select",style = MaterialTheme.typography.titleMedium,fontSize = 12.sp, color = colors.onBackground60)
-                        Text(text = "3",style = MaterialTheme.typography.titleMedium,fontSize = 16.sp, color = colors.onBackground87)
+                        Text(
+                            text = "Total select",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontSize = 12.sp,
+                            color = colors.onBackground60
+                        )
+                        Text(
+                            text = state.categoriesSelected.toString(),
+                            style = MaterialTheme.typography.titleMedium,
+                            fontSize = 16.sp,
+                            color = colors.primary
+                        )
                     }
-                    Button(onClick = { } , contentPadding = PaddingValues(horizontal =60.dp), colors = ButtonDefaults.buttonColors(containerColor = colors.primary)) {
-                        Text(text = "Next",style = MaterialTheme.typography.titleMedium,fontSize = 14.sp, color = Color.White)
+                    Button(
+                        onClick = { },
+                        contentPadding = PaddingValues(horizontal = 60.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = colors.primary)
+                    ) {
+                        Text(
+                            text = "Next",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontSize = 14.sp,
+                            color = Color.White
+                        )
                     }
                 }
             }
         }
     )
-    {padding->
+    { padding ->
         LazyVerticalGrid(
-            contentPadding = PaddingValues(top = padding.calculateTopPadding() , bottom = padding.calculateBottomPadding() , start = 16.dp , end = 16.dp) ,
+            contentPadding = PaddingValues(
+                top = padding.calculateTopPadding().plus(16.dp),
+                bottom = padding.calculateBottomPadding().plus(16.dp),
+                start = 16.dp,
+                end = 16.dp
+            ),
             columns = GridCells.Fixed(count = 2),
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(
-                items = categories
+                items = state.categories
             ) { category ->
-                CategoryCard(category = category)
+                CategoryCard(category = category,
+                    onClick = {
+                        onCategorySelected(category)
+                    })
             }
         }
     }
 
 
-
-
 }
 
-@Preview()
+@Preview
 @Composable
 fun CategoryScreenPreview() {
-    CategoryScreen()
+    TriviaTitansTheme {
+        CategoryScreen()
+    }
 }
