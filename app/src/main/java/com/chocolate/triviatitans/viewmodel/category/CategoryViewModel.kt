bee3bdb97ad2cbb3dcf79c0e9ee07f41c46de837
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-07-07 22:45:19 +0300

    update category viewModel

--- app/src/main/java/com/chocolate/triviatitans/viewmodel/category/CategoryViewModel.kt
+++ app/src/main/java/com/chocolate/triviatitans/viewmodel/category/CategoryViewModel.kt
@@ -1,6 +1,5 @@
 package com.chocolate.triviatitans.viewmodel.category
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.chocolate.triviatitans.R
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -25,13 +24,20 @@ class CategoryViewModel @Inject constructor(
         _state.update { it.copy(categories = categories()) }
     }
 
-    fun onCategorySelected(categoryUiState: CategoryUiState, isSelected: Boolean) {
-        _state.update { categoriesUiState ->
-            categoriesUiState.copy(
-                categoriesSelected = categoriesUiState.categoriesSelected.let {
-                    if (isSelected) it + categoryUiState
-                    else  it - categoryUiState
-                }
+    fun onCategorySelected(categoryUiState: CategoryUiState) {
+        _state.update {
+            it.copy(
+                categoriesSelected = it.categoriesSelected + categoryUiState,
+                categoriesSelectedCount = it.categoriesSelectedCount + 1
+            )
+        }
+    }
+
+    fun onCategoryDeselected(categoryUiState: CategoryUiState) {
+        _state.update {
+            it.copy(
+                categoriesSelected = it.categoriesSelected - categoryUiState,
+                categoriesSelectedCount = it.categoriesSelectedCount - 1
             )
         }
     }
@@ -44,7 +50,11 @@ class CategoryViewModel @Inject constructor(
             CategoryUiState("film_and_tv", "Movie and Tv", R.drawable.category_movies),
             CategoryUiState("arts_and_literature", "Arts and literature", R.drawable.category_art),
             CategoryUiState("history", "History", R.drawable.category_history),
-            CategoryUiState("society_and_culture", "Society and culture", R.drawable.category_society),
+            CategoryUiState(
+                "society_and_culture",
+                "Society and culture",
+                R.drawable.category_society
+            ),
             CategoryUiState("science", "Science", R.drawable.category_science),
             CategoryUiState("general_knowledge", "General Knowledge", R.drawable.category_general),
             CategoryUiState("food_and_drink", "Food And Drink", R.drawable.category_food),
