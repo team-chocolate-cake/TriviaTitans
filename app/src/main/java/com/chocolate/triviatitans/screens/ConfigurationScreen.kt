4e20415ae130c4431b5b0146e492613e434815bd
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-07 22:09:20 +0300

    update configuration screen

--- app/src/main/java/com/chocolate/triviatitans/screens/ConfigurationScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/screens/ConfigurationScreen.kt
@@ -20,6 +20,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -28,99 +32,113 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.chocolate.triviatitans.screens.configuration.ConfigurationCard
 import com.chocolate.triviatitans.R
-import com.chocolate.triviatitans.composables.ConfigurationCard
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
 import com.chocolate.triviatitans.ui.theme.TriviaTitansTheme
 
 @Composable
 fun ConfigurationScreen(){
+    TriviaTitansTheme() {
+        ConfigurationContent()
+    }
+}
 
-TriviaTitansTheme() {
+@Composable
+fun ConfigurationContent(){
+
+    var selectedIndex by remember {
+        mutableStateOf(-1)
+    }
+
     Column(modifier = Modifier
         .fillMaxSize()
         .background(TriviaCustomColors.current.background)
         .padding(16.dp)) {
-    Text(text = stringResource(id = R.string.game_type), modifier = Modifier.padding(vertical = 8.dp),
-        fontFamily = MaterialTheme.typography.titleMedium.fontFamily
-    , fontSize = 16.sp
-    , color = TriviaCustomColors.current.onBackground87)
+
+        Text(text = stringResource(id = R.string.game_type), modifier = Modifier.padding(vertical = 8.dp),
+            fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+            , fontSize = 16.sp
+            , color = TriviaCustomColors.current.onBackground87)
+
+        ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card1)
+            , typeDescription = stringResource(id = R.string.config_content_card1)
+            , typeImage = R.drawable.configuratoin_multi_choice_icon
+            , currentIndex = 1,
+            index = selectedIndex,
+            selected={i-> selectedIndex=i})
+
+        ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card2)
+            , typeDescription = stringResource(id = R.string.config_content_card2)
+            , typeImage = R.drawable.configuratoin_multi_choice_images_icon,
+            currentIndex = 2,
+            index = selectedIndex,
+            selected={i-> selectedIndex=i})
+
+        ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card3)
+            , typeDescription = stringResource(id = R.string.config_content_card3)
+            , typeImage = R.drawable.configuratoin_word_wise_icon,
+            currentIndex = 3,
+            index = selectedIndex,
+            selected={i-> selectedIndex=i})
 
 
-    ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card1)
-        , typeDescription = stringResource(id = R.string.config_content_card1)
-        , typeImage =R.drawable.configuratoin_multi_choice_icon)
-
-    ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card2)
-        , typeDescription = stringResource(id = R.string.config_content_card2)
-        , typeImage =R.drawable.configuratoin_multi_choice_images_icon )
-
-    ConfigurationCard(typeTitle = stringResource(id = R.string.config_title_card3)
-        , typeDescription = stringResource(id = R.string.config_content_card3)
-        , typeImage =R.drawable.configuratoin_word_wise_icon )
 
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(top = 8.dp)
-        .height(120.dp),
-        colors = CardDefaults.cardColors(TriviaCustomColors.current.onSecondary)
-    ) {
-        Row(modifier = Modifier.fillMaxSize(), Arrangement.SpaceBetween,
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp)
+            .height(120.dp),
+            colors = CardDefaults.cardColors(TriviaCustomColors.current.onSecondary)
         ) {
-            Column(modifier = Modifier
-                .fillMaxHeight()
-                .weight(0.5f)
-                .padding(start = 12.dp),
-                verticalArrangement = Arrangement.Center) {
-                Text(text = stringResource(id = R.string.config_title_card4)
-                    , fontFamily = MaterialTheme.typography.titleMedium.fontFamily
-                ,fontSize = 16.sp
-                ,color = TriviaCustomColors.current.primary)
+            Row(modifier = Modifier.fillMaxSize(), Arrangement.SpaceBetween,
+            ) {
+                Column(modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(0.5f)
+                    .padding(start = 12.dp),
+                    verticalArrangement = Arrangement.Center) {
+                    Text(text = stringResource(id = R.string.config_title_card4)
+                        , fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+                        ,fontSize = 16.sp
+                        ,color = TriviaCustomColors.current.primary)
 
-                Text(text = stringResource(id = R.string.config_content_card4)
-                    , maxLines = 3,
-                    textAlign = TextAlign.Justify,
-                    fontFamily = MaterialTheme.typography.titleLarge.fontFamily
-                ,fontSize = 22.sp
-                ,color = TriviaCustomColors.current.primary)
+                    Text(text = stringResource(id = R.string.config_content_card4)
+                        , maxLines = 3,
+                        textAlign = TextAlign.Justify,
+                        fontFamily = MaterialTheme.typography.titleLarge.fontFamily
+                        ,fontSize = 22.sp
+                        ,color = TriviaCustomColors.current.primary)
+
+                }
+                Spacer(Modifier.size( 24.dp))
+                Image(modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(end = 12.dp)
+                    ,painter = painterResource(id = R.drawable.configuratoin_coming_soon_icon),
+                    contentDescription =null )
+            }
+        }
+
+
+        if(selectedIndex!=-1){
+            Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .width(250.dp)
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 38.dp),
+                colors = ButtonDefaults.buttonColors(TriviaCustomColors.current.primary),
+
+                ) {
+                Text(text = stringResource(id = R.string.play_now),
+                    fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+                    ,fontSize = 20.sp)
 
             }
-            Spacer(Modifier.size( 24.dp))
-            Image(modifier = Modifier
-                .fillMaxHeight()
-                .padding(end = 12.dp)
-                ,painter = painterResource(id = R.drawable.configuratoin_coming_soon_icon),
-                contentDescription =null )
         }
-    }
-
-
-
-    Button(onClick = { /*TODO*/ }, modifier = Modifier
-        .width(250.dp)
-        .align(Alignment.CenterHorizontally)
-        .padding(top=38.dp),
-        colors = ButtonDefaults.buttonColors(TriviaCustomColors.current.primary)
-    ) {
-        Text(text = stringResource(id = R.string.play_now),
-            fontFamily = MaterialTheme.typography.titleMedium.fontFamily
-        ,fontSize = 20.sp)
 
     }
 
 }
-
-
-
-
-
-
-    }
-
-
-}
-
 
 
 @Preview(showBackground = true, showSystemUi = true)
--- app/src/main/java/com/chocolate/triviatitans/screens/configuration/ConfigurationCard.kt
+++ app/src/main/java/com/chocolate/triviatitans/screens/configuration/ConfigurationCard.kt
@@ -1,6 +1,8 @@
-package com.chocolate.triviatitans.composables
+package com.chocolate.triviatitans.screens.configuration
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,15 +30,22 @@ import com.chocolate.triviatitans.R
 import com.chocolate.triviatitans.ui.theme.TriviaCustomColors
 
 @Composable
-fun ConfigurationCard(typeTitle:String,typeDescription:String,typeImage: Int,
-color: Color = TriviaCustomColors.current.card
+fun ConfigurationCard(typeTitle:String,
+                      typeDescription:String,
+                      typeImage: Int,
+color: Color = TriviaCustomColors.current.card,
+                      currentIndex:Int,
+                      index:Int,
+                      selected:(Int)->Unit
 ){
 
        Card(modifier = Modifier
            .fillMaxWidth()
            .padding(top = 8.dp)
-           .height(120.dp),
-           colors = CardDefaults.cardColors(color)
+           .height(120.dp).
+           clickable { selected(currentIndex)},
+           colors = CardDefaults.cardColors(color),
+           border = if(currentIndex==index) BorderStroke(2.dp, TriviaCustomColors.current.primary) else null
        ) {
            Row(modifier = Modifier.fillMaxSize(), Arrangement.SpaceBetween,
            ) {
@@ -73,6 +83,6 @@ color: Color = TriviaCustomColors.current.card
 @Composable
 fun PreviewConfigurationCard(){
     ConfigurationCard("test",
-        "desc",
-        R.drawable.configration_multi_choice_images_icon)
+        "test2",
+        R.drawable.configration_multi_choice_images_icon,currentIndex=3,index=0){}
 }
