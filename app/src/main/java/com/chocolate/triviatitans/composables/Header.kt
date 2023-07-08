1547ca1e32c2efd1b758dcd52cb4eeb3c1f6c5a9
Saif Alhaider <saifharith90@gmail.com>
2023-07-08 12:35:47 +0300

    add: pass header question info

--- app/src/main/java/com/chocolate/triviatitans/composables/Header.kt
+++ app/src/main/java/com/chocolate/triviatitans/composables/Header.kt
@@ -1,21 +1,38 @@
-package com.chocolate.triviatitans.presentation.screens.quiz_screen.components
+package com.chocolate.triviatitans.composables
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.chocolate.triviatitans.composables.SpacerVertical16
-import com.chocolate.triviatitans.composables.SpacerVertical32
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.PlayerDashBoard
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.ProgressIndicator
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.components.QuestionHintsSection
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.listener.HintListener
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.viewModel.MultiChoiceTextUiState
 import com.chocolate.triviatitans.presentation.theme.TriviaCustomColors
 import com.chocolate.triviatitans.presentation.theme.TriviaTitansTheme
 
 @Composable
-fun Header(question:String) {
+fun Header(
+    question: String,
+    hintListener: HintListener,
+    fiftyHint: MultiChoiceTextUiState.HintButton,
+    heartHint: MultiChoiceTextUiState.HintButton,
+    resetHint: MultiChoiceTextUiState.HintButton,
+    questionNumber: Int,
+    userScore: Int,
+    correctAnswer: String
+) {
     Column {
-        PlayerDashBoard()
+        PlayerDashBoard(questionNumber, userScore)
         SpacerVertical16()
-        QuestionHintsSection()
+        QuestionHintsSection(
+            hintListener = hintListener, fiftyHint,
+            heartHint,
+            resetHint,
+            correctAnswer
+        )
         SpacerVertical16()
         ProgressIndicator(progressPercentage = .6f)
         SpacerVertical32()
@@ -30,6 +47,23 @@ fun Header(question:String) {
 @Preview(showSystemUi = true)
 @Composable
 fun HeaderPreview() {
-    TriviaTitansTheme() { Header("") }
+    TriviaTitansTheme() {
+        Header(
+            "", hintListener = object : HintListener {
+                override fun onClickFiftyFifty() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onClickHeart() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onClickReset() {
+                    TODO("Not yet implemented")
+                }
+            }, MultiChoiceTextUiState.HintButton(), MultiChoiceTextUiState.HintButton(),
+            MultiChoiceTextUiState.HintButton(), questionNumber = 3, userScore = 0, ""
+        )
+    }
 }
 
