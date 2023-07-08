3aeac0faeef4dc2d2276ed00e976641680d86bc1
Saif Alhaider <saifharith90@gmail.com>
2023-07-08 12:34:49 +0300

    add:hint button listener

---
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/listener/HintListener.kt
@@ -0,0 +1,7 @@
+package com.chocolate.triviatitans.presentation.screens.quiz_screen.listener
+
+interface HintListener{
+    fun onClickFiftyFifty()
+    fun onClickHeart()
+    fun onClickReset()
+}
