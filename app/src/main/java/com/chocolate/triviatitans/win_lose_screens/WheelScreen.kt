660aa1142ae07d625c1f928ce81eed062d72f046
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-08 18:48:03 +0300

    create spin

--- app/src/main/java/com/chocolate/triviatitans/win_lose_screens/WheelScreen.kt
+++ app/src/main/java/com/chocolate/triviatitans/win_lose_screens/WheelScreen.kt
@@ -1,10 +1,13 @@
 package com.chocolate.triviatitans.win_lose_screens
 
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,6 +16,7 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.chocolate.triviatitans.R
 import com.chocolate.triviatitans.presentation.theme.LightOnBackground87
+import com.chocolate.triviatitans.win_lose_screens.composable.Wheel
 
 @Preview(showSystemUi = true)
 @Composable
@@ -33,11 +37,18 @@ fun SpinWheel() {
                 },
             textAlign = TextAlign.Center,
             fontSize = 22.sp,
-            color = LightOnBackground87,
-
-            )
+            color = LightOnBackground87
+        )
+        Wheel(
+            modifier = Modifier.constrainAs(wheel) {
+                top.linkTo(spinText.bottom, margin = 90.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            context = LocalContext.current
+        )
     }
-
-
-
 }
+
