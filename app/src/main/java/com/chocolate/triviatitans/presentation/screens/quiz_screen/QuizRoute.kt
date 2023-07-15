0f744d5285f900168f62223036b4fc0f1cf3f331
mohammed-akkad <momoo362514@gmail.com>
2023-07-15 11:53:24 +0300

    refactor quiz route

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/QuizRoute.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/QuizRoute.kt
@@ -1,5 +1,6 @@
 package com.chocolate.triviatitans.presentation.screens.quiz_screen
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,14 +8,18 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.chocolate.triviatitans.presentation.Screens
+import com.chocolate.triviatitans.presentation.screens.category.CategoryArgs.Companion.CURRENT_INDEX
+import com.chocolate.triviatitans.presentation.screens.level.LevelArgs.Companion.CATEGORIES
+import com.chocolate.triviatitans.presentation.screens.level.LevelArgs.Companion.GAME_TYPE
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.QuizArgs.Companion.LEVEL_TYPE
 
 fun NavGraphBuilder.quizRoute(navController: NavHostController) {
     composable(
-        "${Screens.QuizScreen.route}/{categories}/{game_type}/{level_type}",
+        "${Screens.QuizScreen.route}/{$CATEGORIES}/{$GAME_TYPE}/{$LEVEL_TYPE}",
         arguments = listOf(
-            navArgument("categories") { NavType.StringType },
-            navArgument("game_type") { NavType.IntType },
-            navArgument("level_type") { NavType.StringType }
+            navArgument(CATEGORIES) { NavType.StringType },
+            navArgument(GAME_TYPE) { NavType.IntType },
+            navArgument(LEVEL_TYPE) { NavType.StringType }
         )
     ) {
         QuizScreen(navController = navController)
@@ -23,4 +28,11 @@ fun NavGraphBuilder.quizRoute(navController: NavHostController) {
 
 fun NavController.navigateToQuiz(categories: String="", gameType: Int=0, levelArgs: String="") {
     navigate("${Screens.QuizScreen.route}/$categories/$gameType/$levelArgs")
+}
+
+class QuizArgs(savedStateHandle: SavedStateHandle){
+    val levelType = checkNotNull(savedStateHandle[LEVEL_TYPE])
+    companion object{
+        const val LEVEL_TYPE = "level_type"
+    }
 }
