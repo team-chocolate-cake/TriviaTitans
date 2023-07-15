0ff0f660678bb6345246dd6de4b9ca0bdef56e2a
mohammed-akkad <momoo362514@gmail.com>
2023-07-15 11:53:15 +0300

    refactor level route

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/level/LevelRoute.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/level/LevelRoute.kt
@@ -1,15 +1,18 @@
 package com.chocolate.triviatitans.presentation.screens.level
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.chocolate.triviatitans.presentation.Screens
+import com.chocolate.triviatitans.presentation.screens.level.LevelArgs.Companion.CATEGORIES
+import com.chocolate.triviatitans.presentation.screens.level.LevelArgs.Companion.GAME_TYPE
 
 fun NavGraphBuilder.levelRoute(navController: NavController) {
     composable(
-        "${Screens.LevelScreen.route}/{categories}/{game_type}",
+        "${Screens.LevelScreen.route}/{$CATEGORIES}/{$GAME_TYPE}",
         arguments = listOf(
             navArgument("categories") { NavType.StringType},
             navArgument("game_type"){NavType.IntType}
@@ -20,4 +23,13 @@ fun NavGraphBuilder.levelRoute(navController: NavController) {
 }
 fun NavController.navigateToLevel(categories: String="",gameType:Int=0) {
     navigate("${Screens.LevelScreen.route}/$categories/$gameType")
+}
+
+class LevelArgs(savedStateHandle: SavedStateHandle){
+    val categories = checkNotNull(savedStateHandle[CATEGORIES])
+    val gameType = checkNotNull(savedStateHandle[GAME_TYPE])
+    companion object{
+        const val CATEGORIES = "categories"
+        const val GAME_TYPE = "game_type"
+    }
 }
