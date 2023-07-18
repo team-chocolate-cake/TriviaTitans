2bb64ef826429fa400180efe1f3affe956e77e83
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-18 23:05:55 +0300

    refactor ProgressIndicator in timer

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/components/multi_choice/ProgressIndicator.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/components/multi_choice/ProgressIndicator.kt
@@ -1,5 +1,9 @@
 package com.chocolate.triviatitans.presentation.screens.quiz_screen.components.multi_choice
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -15,14 +20,27 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProgressIndicator(progressPercentage:Float,modifier: Modifier=Modifier) {
+    val timer by animateFloatAsState(targetValue = progressPercentage)
+
+    val progressAnimDuration = 1500
+
+    val color by animateColorAsState(if(timer<0.7f) Color(0xFFF21B1B)  else  Color(0xFF09C411)
+        , tween(durationMillis = progressAnimDuration, easing = FastOutSlowInEasing)
+    )
     Box(Modifier.clip(RoundedCornerShape(4.dp))){
-        Spacer(modifier = Modifier.fillMaxWidth().height(8.dp).background(Color.White))
-        Spacer(modifier = Modifier.fillMaxWidth(progressPercentage).height(8.dp).background(Color(0xFFF21B1B)))
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(8.dp)
+            .background(Color.White))
+        Spacer(modifier = Modifier
+            .fillMaxWidth(timer)
+            .height(8.dp)
+            .background(color))
     }
 }
 
 @Preview
 @Composable
 fun ProgressIndicatorPreview() {
-    ProgressIndicator(.5f)
+    ProgressIndicator(.7f)
 }
