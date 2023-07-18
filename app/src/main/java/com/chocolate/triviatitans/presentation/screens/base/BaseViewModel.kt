f4e3f139a3bbf93b9cb7d734f0c42b45e6243ebe
mohammed-akkad <momoo362514@gmail.com>
2023-07-18 14:59:09 +0300

    add base view model

---
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/base/BaseViewModel.kt
@@ -0,0 +1,25 @@
+package com.chocolate.triviatitans.presentation.screens.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+abstract class BaseViewModel : ViewModel() {
+    fun <T> tryToExecute(
+        call: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (Throwable) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ) {
+        viewModelScope.launch(dispatcher) {
+            try {
+                call().also(onSuccess)
+            } catch (throwable: Throwable) {
+                onError(throwable)
+            }
+        }
+    }
+}
