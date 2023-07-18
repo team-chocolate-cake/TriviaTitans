17f40bcc7da73ed903293644ec654d459f2e235d
mohammed-akkad <momoo362514@gmail.com>
2023-07-18 15:56:05 +0300

    add word wide game args

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/view_model/WordWiseViewModel.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/view_model/WordWiseViewModel.kt
@@ -1,11 +1,14 @@
 package com.chocolate.triviatitans.presentation.screens.quiz_screen.word_wise.view_model
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chocolate.triviatitans.domain.entities.TextChoiceEntity
 import com.chocolate.triviatitans.domain.usecase.GetMultiChoiceTextGameUseCase
 import com.chocolate.triviatitans.presentation.screens.base.BaseViewModel
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.text_game.TextGameArgs
+import com.chocolate.triviatitans.presentation.screens.quiz_screen.word_wise.WordWiseGameArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -17,11 +20,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class WordWiseViewModel @Inject constructor(
-    private val getMultiChoiceTextGameUseCase: GetMultiChoiceTextGameUseCase
+    private val getMultiChoiceTextGameUseCase: GetMultiChoiceTextGameUseCase,
+    savedStateHandle: SavedStateHandle
 ) :
     BaseViewModel() {
     private val _state = MutableStateFlow(WordWiseUIState())
     val state = _state.asStateFlow()
+
+    private val textGameArgs: WordWiseGameArgs = WordWiseGameArgs(savedStateHandle)
+
 
     init {
         getUserQuestions()
