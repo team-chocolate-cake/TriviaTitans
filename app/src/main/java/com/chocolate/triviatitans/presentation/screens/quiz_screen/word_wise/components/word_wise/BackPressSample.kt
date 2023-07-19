d89b356a8a4f0cfd95dd56d50f85ce8104765e67
Mahmoud Elboray <ma5126@fayoum.edu.eg>
2023-07-20 01:24:35 +0300

    add dialog and do refactor

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/components/word_wise/BackPressSample.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/components/word_wise/BackPressSample.kt
@@ -1,6 +1,5 @@
 package com.chocolate.triviatitans.presentation.screens.quiz_screen.word_wise.components.word_wise
 
-import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,7 +7,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.chocolate.triviatitans.presentation.theme.LightError
+import com.chocolate.triviatitans.presentation.theme.OnPrimary
 import kotlinx.coroutines.delay
 
 @Composable
@@ -16,11 +17,14 @@ fun BackPressSample() {
     var showToast by remember { mutableStateOf(false) }
 
     var backPressState by remember { mutableStateOf<BackPress>(BackPress.Idle) }
-    val context = LocalContext.current
 
-    if(showToast){
-        Toast.makeText(context, "Press again to exit", Toast.LENGTH_SHORT).show()
-        showToast= false
+    if (showToast) {
+        ExitDialogBox(
+            OnPrimary,
+            LightError,
+            18.dp,
+        )
+        showToast = true
     }
 
 
