ce6fd9db43b10187ec3d07c52662b10d44c20cde
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-07-19 23:51:23 +0300

    delete unnecessary vals in WordWiseUIState

--- app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/view_model/WordWiseUIState.kt
+++ app/src/main/java/com/chocolate/triviatitans/presentation/screens/quiz_screen/word_wise/view_model/WordWiseUIState.kt
@@ -6,10 +6,8 @@ data class WordWiseUIState(
     val isLoading: Boolean = true,
     val questionUiStates: List<QuestionUiState> = emptyList(),
     val error: String? = null,
-    val isEmpty: Boolean = false,
     val userScore: Int = 0,
     val questionNumber: Int = 0,
-    val levelType: String = "",
     val selectedLetterList: List<Char> = emptyList(),
     val keyboardLetters: List<Char> = emptyList(),
     val hintFiftyFifty: HintButton = HintButton(),
@@ -22,7 +20,6 @@ data class WordWiseUIState(
     data class QuestionUiState(
         val id: String = "",
         val question: String = "",
-        val category: String = "",
         val difficulty: String = "",
         val correctAnswer: String,
         val correctAnswerLetters: List<Char> = emptyList()
